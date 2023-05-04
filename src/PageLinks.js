315c741968e3200c8a7005b55a2cbbac2be4eaea
arivarton <accounts@arivarton.com>
2023-05-04 17:06:46 +0200

    Laget dropdown med linker som kan endres i src/PageLinks.js. navbarItems ble flyttet til src/PageLinks.js.

---
+++ src/PageLinks.js
@@ -0,0 +1,9 @@
+export const navbarItems = [
+  { name: 'HJEM', path: '/grupper/D05-23' },
+  { name: 'OM OSS', path: '/grupper/D05-23/omoss#' },
+  /*{ name: 'KONTAKT', path: '/kontakt#' },*/
+];
+export const navDocItems = [
+  { name: 'GUI (Publiseres snart)', path: '#', id: 3 },
+  { name: 'Rapport (Publiseres 22 mai)', path: '#', id: 4 },
+];
