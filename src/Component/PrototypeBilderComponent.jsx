eb2249988d9d4a8fd43fa11e6e778ecd9b373ad4
arivarton <accounts@arivarton.com>
2023-05-05 13:59:45 +0200

    Lagt til galler med modal for prototype.

--- src/Component/PrototypeBilderComponent.jsx
+++ src/Component/PrototypeBilderComponent.jsx
@@ -1,58 +1,28 @@
-import proto1 from '../img/prototype/Andri (16) edit.jpg';
-import proto2 from '../img/prototype/Andri (18) edit.jpg';
-import proto3 from '../img/prototype/Andri (21) edit.jpg';
-import proto4 from '../img/prototype/Andri (22) edit.jpg';
-import proto5 from '../img/prototype/Andri (23) edit.jpg';
-import proto6 from '../img/prototype/Andri (24) edit.jpg';
+import * as React from 'react';
 
-const posts = [
-  {
-    id: 1,
-    imageUrl:
-      proto6,
-  },
-  {
-    id: 2,
-    imageUrl:
-      proto2,
-  },
-  {
-    id: 3,
-    imageUrl:
-      proto3,
-  },
-  {
-    id: 4,
-    imageUrl:
-      proto4,
-  },
-  {
-    id: 5,
-    imageUrl:
-      proto5,
-  },
-  {
-    id: 6,
-    imageUrl:
-      proto1,
-  },
-]
+import PhotoAlbum from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
+
+import slides from '../data/prototype_bilder';
+import "yet-another-react-lightbox/styles.css";
 
 export default function PrototypeBilder() {
+  const [index, setIndex] = React.useState(-1);
+
   return (
-  <div className="py-0 sm:py-0">
-    <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <div className="mx-auto mt-0 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-        {posts.map((post) => (
-          <article
-            key={post.id}
-            className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80"
-          >
-            <img src={post.imageUrl} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
-          </article>
-        ))}
-      </div>
-    </div>
-  </div>
+    <>
+      <PhotoAlbum
+          layout="rows"
+          photos={slides}
+          targetRowHeight={150}
+          onClick={({ index }) => setIndex(index)}
+        />
+        <Lightbox
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+          slides={slides}
+        />
+    </>
   )
 }
